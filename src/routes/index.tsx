3a0f37c7e2fa7c03dd9888b2ccbdbe99bf92c626
kangbeomsu <kbs21c1987@gmail.com>
2026-01-09 16:18:01 +0900

    feat: base path 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div></div>;
+  return (
+    <div>
+      <Link
+        to={"/previews/$domain/$slug/$exampleId"}
+        params={{
+          domain: "html",
+          slug: "what-is-html",
+          exampleId: "ex-important",
+        }}
+      >
+        Go to /previews/$domain/$slug/$exampleId
+      </Link>
+    </div>
+  );
 }
