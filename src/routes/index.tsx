1931dc546b300c6fe1b10b3f80b679228547ed78
kangbeomsu <kbs21c1987@gmail.com>
2026-01-10 11:31:05 +0900

    fix: git pages 오류 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,5 +20,3 @@ function RouteComponent() {
     </div>
   );
 }
-
-
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     tanstackRouter({
       target: "react",
-      autoCodeSplitting: true,
+      autoCodeSplitting: false,
     }),
     react({
       babel: {
